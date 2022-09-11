bfba8e58afe42ee06e18aecdcd13eb1de58a445b
abhikharat <abhijeetkharat0107@gmail.com>
2022-09-11 21:57:42 +0530

    Added Base64 url encoder/decoder.

--- Java8Demos/src/main/java/com/abhi/java8demos/base64/Base64Example.java
+++ Java8Demos/src/main/java/com/abhi/java8demos/base64/Base64Example.java
@@ -32,7 +32,27 @@ public class Base64Example {
         System.out.println("Decoded String : "+str2);
 
 
+        //Url Encoding decoding
+        //Getting encoder
+        Base64.Encoder urlEncoder=Base64.getUrlEncoder();
+        String strEncoder=urlEncoder.encodeToString("https://www.facebook.com/".getBytes());
+        System.out.println("Encoded Url : "+strEncoder);
+        //Getting decoder
+        Base64.Decoder urlDecoder=Base64.getUrlDecoder();
+        String strDecoder=new String(urlDecoder.decode(strEncoder));
+        System.out.println("Decoded url : "+strDecoder);
 
+        // Getting MIME encoder
+        Base64.Encoder encoder3 = Base64.getMimeEncoder();
+        String message = "Hello, \nYou are informed regarding your inconsistency of work";
+        String eStr = encoder3.encodeToString(message.getBytes());
+        System.out.println("Encoded MIME message: "+eStr);
+
+        // Getting MIME decoder
+        Base64.Decoder decoder3 = Base64.getMimeDecoder();
+        // Decoding MIME encoded message
+        String dStr = new String(decoder3.decode(eStr));
+        System.out.println("Decoded message: "+dStr);
 
     }
 }
