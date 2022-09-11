5289ab8af40df05e94afa79d76e627e05466c57f
abhikharat <abhijeetkharat0107@gmail.com>
2022-09-11 21:34:23 +0530

    Added base64 class

---
+++ Java8Demos/src/main/java/com/abhi/java8demos/base64/Base64Example.java
@@ -0,0 +1,38 @@
+package com.abhi.java8demos.base64;
+
+import java.util.Base64;
+
+public class Base64Example {
+
+    public static void main(String[] args){
+
+        //Getting encoder
+        Base64.Encoder encoder=Base64.getEncoder();
+
+        //Creating byte array
+        byte[] byteArray={1,2};
+        //Encoder byte array
+       byte[] byteArray2= encoder.encode(byteArray);
+       System.out.println("Encoded byte array : "+byteArray2);
+       //Create new byte array with enough size
+        byte[] byteArray3=new byte[5];
+        int intx=encoder.encode(byteArray,byteArray3); //Return no of byte written
+        System.out.println("Encoded byte array written in other byte array : "+byteArray3);
+        System.out.println("Number of byte written : "+intx);
+
+
+        //Encoding string
+        String str=encoder.encodeToString("Abhijeet Kharat".getBytes());
+        System.out.println("Encoded string  : "+str);
+
+        //Getting decoded
+        Base64.Decoder decoder=Base64.getDecoder();
+        //Decoding string
+        String str2=new String(decoder.decode(str));
+        System.out.println("Decoded String : "+str2);
+
+
+
+
+    }
+}
